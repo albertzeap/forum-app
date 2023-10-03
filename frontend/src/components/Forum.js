fdf3369fb2cfb36fdc7d2320e1b58a6f4a71244f
G0dzilla21 <reevesc2@vcu.edu>
2023-10-03 11:40:22 -0400

    login and signup working

---
+++ frontend/src/components/Forum.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Forum = () => {
+    return (
+        <div>
+            <h2>Main Forum Page</h2>
+            {/* Add your main forum content here */}
+        </div>
+    );
+};
+
+export default Forum;
