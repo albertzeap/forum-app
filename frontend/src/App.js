88b2bf169a4bbf820740fe1481dcad2ce7b996b5
jimvic37@github.com <Ethan>
2023-10-03 21:39:27 -0500

    commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Signup from './components/Signup';
 import Login from './components/Login';
-import { Forum } from './components/forum/Forum';
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-
+import Forum from './components/forum/Forum';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
 const App = () => {
-    return (
-        <BrowserRouter>
-           
-            <Routes>
-                <Route path="/" exact element={<Forum/>}/>
+  return (
+    <BrowserRouter>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            Forum App
+          </Link>
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">
+                Login
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/signup">
+                Sign Up
+              </Link>
+            </li>
+            {/* You can add a Logout link here when implementing authentication */}
+          </ul>
+        </div>
+      </nav>
 
-            </Routes>
-        
-        </BrowserRouter>
-            
-    );
+      <Routes>
+        <Route path="/" element={<Forum />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- frontend/src/components/forum/Forum.js
+++ frontend/src/components/forum/Forum.js
@@ -191,3 +191,4 @@ export const Forum = () => {
     );
 };
 
+export default Forum;
